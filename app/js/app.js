8c7d3a6427901f8d06fa0e2a9aa6b793cda3a758
Naba Sadia Siddiqui <naba.sadia.siddiqui@gmail.com>
2016-01-10 19:13:56 -0500

    Refactor angular seed project-provided resources
    - Remove resources (JS source files, css, etc.) not needed by the
      project
    - Update index.html with the required content

--- app/js/app.js
+++ app/js/app.js
@@ -1,11 +1,8 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
+angular.module('youtubeToMp3Converter', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
